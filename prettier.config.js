37681b134283aa03cc469b7000759242102dc3b9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-08 14:49:07 -0500

    Yay

--- prettier.config.js
+++ prettier.config.js
@@ -3,26 +3,26 @@
  * @type {import("prettier").Config}
  */
 const config = {
-	plugins: ['prettier-plugin-ember-template-tag'],
-	overrides: [
-		{
-			// Lol, JavaScript
-			files: ['*.js', '*.ts', '*.cjs', '.mjs', '.cts', '.mts', '.cts'],
-			options: {
-				singleQuote: true,
-				trailingComma: 'es5',
-			},
-		},
-		{
-			files: ['*.gjs', '*.gts'],
-			options: {
-				parser: 'ember-template-tag',
-				singleQuote: true,
-				templateSingleQuote: false,
-				trailingComma: 'es5',
-			},
-		},
-	],
+  plugins: ['prettier-plugin-ember-template-tag'],
+  overrides: [
+    {
+      // Lol, JavaScript
+      files: ['*.js', '*.ts', '*.cjs', '.mjs', '.cts', '.mts', '.cts'],
+      options: {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    },
+    {
+      files: ['*.gjs', '*.gts'],
+      options: {
+        parser: 'ember-template-tag',
+        singleQuote: true,
+        templateSingleQuote: false,
+        trailingComma: 'es5',
+      },
+    },
+  ],
 };
 
 export default config;
